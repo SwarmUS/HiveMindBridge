39e4c103090e7fb269d3bc171138023b4d5a5b50
Samuel Laperriere <s.t.laperriere@gmail.com>
2021-04-06 17:10:23 -0400

    propolis-pheromones seems to work

---
+++ include/HiveMindBridge/HiveMindBridge.h
@@ -0,0 +1,47 @@
+#ifndef HIVEMIND_BRIDGE_HIVEMINDBRIDGE_H
+#define HIVEMIND_BRIDGE_HIVEMINDBRIDGE_H
+#include "HiveMindBridge/HiveMindBridgeImpl.h"
+#include "HiveMindBridge/IHiveMindBridge.h"
+#include "HiveMindBridge/MessageHandler.h"
+#include "HiveMindBridge/OutboundRequestHandle.h"
+#include "HiveMindBridge/TCPServer.h"
+#include "HiveMindBridge/ThreadSafeQueue.h"
+#include <cpp-common/ILogger.h>
+#include <hivemind-host/HiveMindHostDeserializer.h>
+#include <hivemind-host/HiveMindHostSerializer.h>
+#include <memory>
+
+class HiveMindBridge : public IHiveMindBridge {
+  public:
+    /**
+     * Construct a HiveMindBridge
+     * @param tcpPort The port that the TCP server should listen to
+     */
+    HiveMindBridge(int tcpPort, ILogger& logger);
+
+    void spin();
+
+    void onConnect(std::function<void()> hook);
+
+    void onDisconnect(std::function<void()> hook);
+
+    bool registerCustomAction(std::string name,
+                              CallbackFunction callback,
+                              CallbackArgsManifest manifest);
+
+    bool registerCustomAction(std::string name, CallbackFunction callback);
+
+    bool queueAndSend(MessageDTO message);
+
+  private:
+    ILogger& m_logger;
+    std::unique_ptr<HiveMindBridgeImpl> m_bridge;
+    TCPServer m_tcpServer;
+    HiveMindHostDeserializer m_deserializer;
+    HiveMindHostSerializer m_serializer;
+    MessageHandler m_messageHandler;
+    ThreadSafeQueue<MessageDTO> m_inboundQueue;
+    ThreadSafeQueue<OutboundRequestHandle> m_outboundQueue;
+};
+
+#endif // HIVEMIND_BRIDGE_HIVEMINDBRIDGE_H
---
+++ include/HiveMindBridge/UserCallbackFunctionWrapper.h
@@ -0,0 +1,26 @@
+#ifndef HIVE_MIND_BRIDGE_USERCALLBACKFUNCTIONWRAPPER_H
+#define HIVE_MIND_BRIDGE_USERCALLBACKFUNCTIONWRAPPER_H
+
+#include "HiveMindBridge/Callback.h"
+#include <functional>
+#include <hivemind-host/FunctionCallArgumentDTO.h>
+#include <hivemind-host/FunctionCallRequestDTO.h>
+#include <hivemind-host/FunctionDescriptionArgumentTypeDTO.h>
+#include <unordered_map>
+
+class UserCallbackFunctionWrapper {
+  public:
+    UserCallbackFunctionWrapper() = default;
+
+    UserCallbackFunctionWrapper(CallbackFunction function, CallbackArgsManifest manifest);
+
+    CallbackFunction getFunction();
+
+    CallbackArgsManifest getManifest();
+
+  private:
+    CallbackFunction m_function;
+    CallbackArgsManifest m_manifest;
+};
+
+#endif // HIVE_MIND_BRIDGE_USERCALLBACKFUNCTIONWRAPPER_H
