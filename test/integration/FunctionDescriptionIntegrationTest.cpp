dbc6ef229d458536963df75bc1b8626175169f87
Samuel Laperriere <s.t.laperriere@gmail.com>
2021-04-14 11:47:01 -0400

    refac function description test

--- test/integration/FunctionDescriptionIntegrationTest.cpp
+++ test/integration/FunctionDescriptionIntegrationTest.cpp
@@ -1,5 +1,6 @@
 #include "../utils/Logger.h"
 #include "../utils/TCPClient.h"
+#include "../utils/HiveMindBridgeFixture.h"
 #include "HiveMindBridge/HiveMindBridge.h"
 #include <gmock/gmock.h>
 #include <pheromones/FunctionListLengthRequestDTO.h>
@@ -10,21 +11,8 @@
 #include <pheromones/UserCallRequestDTO.h>
 #include <thread>
 
-bool g_threadShouldRun = true;
-
-class FunctionDescriptionRequestIntegrationTestFixture : public testing::Test {
+class FunctionDescriptionRequestIntegrationTestFixture : public testing::Test, public HiveMindBridgeFixture {
   protected:
-    Logger m_logger;
-    int m_tcpPort = 5001;
-
-    // Bridge side
-    HiveMindBridge* m_bridge;
-
-    // Client side
-    TCPClient* m_tcpClient;
-    HiveMindHostSerializer* m_clientSerializer;
-    HiveMindHostDeserializer* m_clientDeserializer;
-
     void setUpCallbacks() {
         // Register custom actions
         CallbackFunction moveByCallback =
@@ -51,65 +39,14 @@ class FunctionDescriptionRequestIntegrationTestFixture : public testing::Test {
         m_bridge->registerCustomAction("getStatus", getStatus);
     }
 
-    void connectClient() {
+    void SetUp() {
         std::this_thread::sleep_for(std::chrono::milliseconds(250));
-        m_tcpClient->connect();
-    }
 
-    void greet() {
-        // Wait for a greet message
-        MessageDTO greetRequest;
-        if (m_clientDeserializer->deserializeFromStream(greetRequest)) {
-            // The client has ID 42
-            MessageDTO greetResponse(42, 42, GreetingDTO(42));
-            m_clientSerializer->serializeToStream(greetResponse);
-        } else {
-            m_logger.log(LogLevel::Warn, "Deserializing greet failed.");
-        }
+        setUpCallbacks();
     }
-
-    void SetUp() { std::this_thread::sleep_for(std::chrono::milliseconds(250)); }
 
     void TearDown(){};
 
-  public:
-    FunctionDescriptionRequestIntegrationTestFixture() {
-        // Bridge side
-        m_bridge = new HiveMindBridge(m_tcpPort, m_logger);
-        m_bridgeThread =
-            std::thread(&FunctionDescriptionRequestIntegrationTestFixture::bridgeThread, this);
-
-        setUpCallbacks();
-
-        // Client side
-        m_tcpClient = new TCPClient(m_tcpPort);
-        m_clientSerializer = new HiveMindHostSerializer(*m_tcpClient);
-        m_clientDeserializer = new HiveMindHostDeserializer(*m_tcpClient);
-
-        connectClient();
-
-        greet();
-    }
-
-    ~FunctionDescriptionRequestIntegrationTestFixture() {
-        g_threadShouldRun = false;
-        m_bridgeThread.join();
-        m_tcpClient->close();
-
-        delete m_tcpClient;
-        delete m_clientSerializer;
-        delete m_clientDeserializer;
-        delete m_bridge;
-    }
-
-    std::thread m_bridgeThread;
-
-    void bridgeThread() {
-        while (g_threadShouldRun) {
-            m_bridge->spin();
-            std::this_thread::sleep_for(std::chrono::milliseconds(250));
-        }
-    }
 };
 
 TEST_F(FunctionDescriptionRequestIntegrationTestFixture, testFunctionListLengthRequest) {
