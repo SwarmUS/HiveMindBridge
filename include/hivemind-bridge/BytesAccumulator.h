df867cac3d916618de52886ff84826663dc05064
Samuel Laperriere <s.t.laperriere@gmail.com>
2021-05-22 10:49:36 -0400

    document

--- include/hivemind-bridge/BytesAccumulator.h
+++ include/hivemind-bridge/BytesAccumulator.h
@@ -4,19 +4,34 @@
 #include <cstdint>
 #include <vector>
 
+/**
+ * An accumulator of bytes to be used for the reception of an arbitrary bytes payload.
+ */
 class BytesAccumulator {
   public:
+    /**
+     * Append some bytes to reconstruct the payload from many packets
+     * @param bytes The bytes from a packet
+     * @param length The length of the packet's payload
+     * @param packetNumber The number of the packet number
+     * @return false if the packet number does not follow strictly the last packet to have been
+     * appended. Returns true if the operation succeded.
+     */
     bool appendBytes(uint8_t* bytes, uint16_t length, uint32_t packetNumber);
 
+    /**
+     * Returns the assembled bytes
+     */
     std::vector<uint8_t> getBytes();
 
+    /**
+     * Returns the number of the last packet that was appended to the accumulator.
+     */
     uint32_t getLastPacketNumber() const;
-
-    void reset();
 
   private:
     std::vector<uint8_t> m_bytes;
-    uint32_t m_lastPacketNumber = -1;
+    uint32_t m_lastPacketNumber = -1; // We start at -1 since the first packet number should be 0
 };
 
 #endif // HIVEMINDBRIDGE_BYTESACCUMULATOR_H
--- include/hivemind-bridge/IHiveMindHostApiRequestHandler.h
+++ include/hivemind-bridge/IHiveMindHostApiRequestHandler.h
@@ -7,9 +7,20 @@
 
 class IHiveMindHostRequestHandler {
   public:
+    /**
+     * Handle a HiveMindHostRequest message type
+     * @param message The encloising message
+     * @param hmRequest The HiveMindHostApiRequest enclosed in the message
+     */
     virtual void handleMessage(const MessageDTO& message,
                                const HiveMindHostApiRequestDTO& hmRequest) = 0;
 
+    /**
+     * Register a user-defined callback to be incoked when the reception of a bytes payload is
+     * completed.
+     * @param callback The user-defined callback to be invoked.
+     * @return True if the callback was ovewritten, false otherwise.
+     */
     virtual bool onBytesReceived(
         std::function<void(uint8_t* bytes, uint64_t bytesLength)> callback) = 0;
 };
